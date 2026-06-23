43e052008943e5de047c3d972487bacb88d046c4
Tim-cryptow <155663393+Tim-cryptow@users.noreply.github.com>
2026-06-23 20:02:31 +0000

    feat: add metadata to anchor and anchors pages

--- web/app/anchors/layout.tsx
+++ web/app/anchors/layout.tsx
@@ -7,6 +7,7 @@ const description =
 export const metadata: Metadata = {
   title,
   description,
+  alternates: { canonical: "/anchors" },
   openGraph: {
     type: "website",
     siteName: "ThesisLock",
